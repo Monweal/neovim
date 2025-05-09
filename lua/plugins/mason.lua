ea553f7f962c4074f7566b3a8c5e8bdf1cb6aae4
Маховик Никита Сергеевич <n.mahovik@prosoftsystems.ru>
2025-05-09 21:39:16 +0500

    make mason packages optional

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,11 +2,11 @@ require("mason").setup()
 
 require("mason-tool-installer").setup({
             ensure_installed = {
-                "clangd",
-                "dockerfile-language-server",
-                "hadolint",
-                "cmake-language-server",
-                "cmakelint",
+                -- "clangd",
+                -- "dockerfile-language-server",
+                -- "hadolint",
+                -- "cmake-language-server",
+                -- "cmakelint",
             },
             auto_update = true,
             run_on_start = true,
